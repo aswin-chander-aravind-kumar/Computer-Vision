06d660a9d20426ab44533c844201acf7759c8264
Aswin Chander Aravindkumar <aravindkumar.a@northeastern.edu>
2024-01-30 11:33:08 -0500

    First Commit

---
+++ CV_Project_1/build/CMakeFiles/faceDetect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/faceDetect.dir/src/faceDetect.cpp.o"
+  "CMakeFiles/faceDetect.dir/src/faceDetect.cpp.o.d"
+  "libfaceDetect.a"
+  "libfaceDetect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/faceDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
